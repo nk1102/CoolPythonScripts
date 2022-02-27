5ec13179eb247a6a9ff05040f5877531147cd5de
Nikunj <nikunjkhandelwal1234@gmail.com>
2022-02-27 13:08:07 +0530

    completed cool-shape class

---
+++ cool-shapes1.py
@@ -0,0 +1,16 @@
+
+# cool turtle like shape
+# created by Nikunj
+
+from turtle import *
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(200):
+    color('red')
+    circle(i)
+    color('orange')
+    circle(i*0.8)
+    right(3)
+    forward(3)
+done()
