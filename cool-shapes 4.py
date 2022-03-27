a6a98eb64f1e54e5b714c0888989bb7d5bd24534
Nikunj <nikunjkhandelwal4614@gmail.com>
2022-03-27 10:08:45 +0530

    Completed cool-shapes 4.py

---
+++ cool-shapes 4.py
@@ -0,0 +1,20 @@
+from tkinter import N
+import turtle
+import colorsys
+
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+t.speed(0)
+n=80
+h=0
+for i in range(302):
+    c=colorsys.hsv_to_rgb(h,1,0.8)
+    h+=1/n
+    t.color(c)
+    t.left(154)
+    t.forward(i)
+    for j in range(3):
+        t.left(55)
+        t.forward(i)
