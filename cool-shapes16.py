4abbf73535810180a40d334a508edabf7a1e0f38
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-02-07 18:38:25 +0530

    Updated Comments

--- cool-shapes16.py
+++ cool-shapes16.py
@@ -1,3 +1,5 @@
+
+# Cool -shapes 16
 import turtle
 import colorsys
 t = turtle.Turtle()
@@ -10,7 +12,7 @@ for i in range(160):
     c = colorsys.hsv_to_rgb(h,1,0.9)
     h += 1/n
     t.color(c)
-    t.left(175)
+    t.left(145)
     for i in range(5):
         t.forward(300)
         t.left(150)
